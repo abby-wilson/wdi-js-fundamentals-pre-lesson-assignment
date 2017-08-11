6cd3ee14a1d2d553d00356feb160cd4556a80df5
Abby Wilson <abigail.wilson1@outlook.com>
2017-08-11 14:53:39 -0400

    finished pacman assignment

---
+++ pacman-console-log.js
@@ -0,0 +1,1 @@
+console.log('Pac-Man easts ghosts for lunch.');
---
+++ pacman-functions.js
@@ -0,0 +1,9 @@
+// Named Function
+function eatDot() {
+  console.log("MUNCH!");
+}
+
+// Anonymous Function
+var eatDot = function() {
+  console.log("MUNCH!");
+}
